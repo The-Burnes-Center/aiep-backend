621d0ef2aeaeaff7839a4e91c954c96517ebd0f7
Kane Xu <xu.hong@northeastern.edu>
2023-11-06 19:03:41 -0500

    CORS MIDDLEWARE ADDED

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ from fastapi import FastAPI
 import uvicorn
 from app.routers import AGO
 from app.utils.logger import logger
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # List of origins allowed (["*"] to allow all origins)
+    allow_credentials=True,  # Allow credentials (cookies, authorization headers, etc)
+    allow_methods=["*"],  # Allow all methods or specify particular methods like ["GET", "POST"]
+    allow_headers=["*"],  # Allow all headers or specify which ones are allowed
+)
 app.include_router(AGO.router)
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level='info')
