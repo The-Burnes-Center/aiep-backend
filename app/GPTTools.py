5299319ca011a90eff39ccfef4b282bb6c11f8e8
Kane Xu <xu.hong@northeastern.edu>
2023-12-10 17:10:10 -0500

    Prototype 2

--- app/GPTTools.py
+++ app/GPTTools.py
@@ -62,7 +62,7 @@ class GPTAssistant:
             instructions=f'{instructions}. Please return the response in {self.language}',
             tools=[{'type': 'retrieval'}],
             model='gpt-4-1106-preview',
-            file_ids=[])
+            file_ids=['file-mwrs7DoUg099Axbs6bWrREpZ'])
         print('A')
         self.assistant_id = assistant.id  # Need Validation
         self.hasBuilt = True
